00620df9586c416c4184f5d5618013f95ab14fdc
jiangxiaobai521 <quanchaoping@outlook.com>
2024-09-28 19:42:45 +0800

    Site updated: 2024-09-28 19:42:43

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/09/28/CentOS8/","2024/09/26/面试/MySQL面试题/","2024/09/26/面试/Redis面试题/","2024/09/26/面试/hashmap源码/","2024/09/26/面试/微服务面试题/","2024/09/26/面试/消息中间件面试题/","2024/09/26/面试/框架篇面试题/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
