0c00bc14df399987f949e44606e28988cf3a256b
jiangxiaobai521 <quanchaoping@outlook.com>
2024-10-18 19:55:41 +0800

    Site updated: 2024-10-18 19:55:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3ef31c5e.html","posts/d8439cde.html","posts/79666db.html"];function toRandomPost(){
+var posts=["posts/73864691.html","posts/3ef31c5e.html","posts/d8439cde.html","posts/a378bd8e.html","posts/79666db.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
